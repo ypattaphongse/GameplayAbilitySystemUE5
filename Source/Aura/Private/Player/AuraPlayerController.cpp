0b18a6b8569332193db956d45302d7a86335f649
ypattaphongse <ypattaphongse@hotmail.com>
2023-06-04 15:31:20 +0700

    Aura Player Controller

---
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,28 @@
+// Copyright Yosan Dev
+
+
+#include "EnhancedInputSubsystems.h"
+#include "Player/AuraPlayerController.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(AuraContext);
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(AuraContext,0);
+
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+}
---
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -0,0 +1,25 @@
+// Copyright Yosan Dev
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+class UInputMappingContext;
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	AAuraPlayerController();
+protected:
+	virtual void BeginPlay() override;
+private:
+	TObjectPtr<UInputMappingContext> AuraContext;
+};
